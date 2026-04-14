baf638803fa83ef0102d344fbbbe29a87a619267
Hailey Ngo <hngo08@calpoly.edu>
2026-04-13 19:59:23 -0700

    divide and conquer
    Added input validation and optimized findSingleton function using binary search.

--- hngo08_lab1.py
+++ hngo08_lab1.py
@@ -1,20 +1,31 @@
 import sys 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python")
+        return
+
     filename = sys.argv[1]
-    with open(filename) as f: 
-        arr = [int(line.strip()) for line in f] 
+    with open(filename) as f:
+        arr = [int(line.strip()) for line in f]
 
     print(findSingleton(arr))
 
-def findSingleton(arr): 
-    i = 0
-    while i < len(arr) - 1: #loop as long as there is a pair in the list 
-        if arr[i] != arr[i +1]: 
-            return arr[i]
-        i += 2 #skip to after a pair
-    return arr[-1] #the last element should be a singleton 
+def findSingleton(arr):
+    lo = 0
+    hi = len(arr) - 1
 
-if __name__ == "__main__": 
+    while lo < hi:
+        mid = (lo + hi) // 2
+
+        if mid % 2 == 1:
+               mid -= 1
+        if arr[mid] == arr[mid + 1]: #the singleton is on the right side
+               lo = mid + 2
+        else: #the singleton is on the left side
+               hi = mid
+
+    return arr[lo]
+
+if __name__ == "__main__":
     main()
-
